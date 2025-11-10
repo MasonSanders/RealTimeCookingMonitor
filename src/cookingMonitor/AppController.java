7323b9d60117cb63a35826e575e50c3b2d510ac3
Mason <masonsanders@gmail.com>
2025-11-10 12:18:31 -0700

    some skeleton methods for AppController and ThermometerDevice and TemperatureListener

---
+++ src/cookingMonitor/AppController.java
@@ -0,0 +1,19 @@
+package cookingMonitor;
+
+public class AppController {
+	public AppController() {
+		// Constructor
+	}
+	
+	public void start() {
+		// The AppController will start the view here.
+	}
+	
+	public void startCooking(String sessionId) {
+		
+	}
+	
+	public void stopCooking(String sessionId) {
+		
+	}
+}
